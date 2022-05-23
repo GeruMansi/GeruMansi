048184a89bc91406a735b0967ce4f85ac79eeea2
Geru <mangeragu@gmail.com>
2022-05-23 01:23:18 -0300

    Corregidos algunos textos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const navBtn = document.getElementById('nav-btn')
 const navLinks = document.getElementById('nav-links')
 const toTop = document.getElementById('to-top')
+const navLogo = document.getElementById('nav-logo')
 
 navBtn.addEventListener('click', () => {
     navLinks.classList.toggle('hidden-links')
@@ -11,6 +12,13 @@ navLinks.addEventListener('click', () => {
 })
 
 toTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
+
+navLogo.addEventListener('click', () => {
     window.scrollTo({
         top: 0,
         behavior: "smooth"
